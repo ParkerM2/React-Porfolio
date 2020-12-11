afc91686bd984362e4026dc523fca51f59d2fc03
Parker Manning <parkerlmanning@hotmail.com>
2020-12-10 22:25:33 -0600

    reverting previous portfolio to react, lots of problems

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+
+
+function App() {
+
+  return (
+      <div>
+        <Portfolio />
+      </div>
+/* <script>
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script> */
+  );
+}
+
+export default App;
